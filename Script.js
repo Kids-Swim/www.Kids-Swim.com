91805580f34b183c05399e4a9b56f899c5520054
Kids-Swim <kidsswimu@gmail.com>
2016-11-13 01:50:34 -0800

    Added Semicolon
    Not sure if this would even help...

--- Script.js
+++ Script.js
@@ -5,5 +5,5 @@ for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
         this.classList.toggle("active");
         this.nextElementSibling.classList.toggle("show");
-  }
+  };
 }
